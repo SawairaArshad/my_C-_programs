64a068561c0515c1b708e0c569545ddd931cd9f7
Sawaira Arshad <110373378+SawairaArshad@users.noreply.github.com>
2022-08-02 10:01:04 +0500

    Create my second_c++programs.cpp

---
+++ my second_c++programs.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() 
+{
+    int valueOne;
+    cout<<"Please enter a value";
+    cin>>valueOne;
+    cout<<"valueOne";// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() 
+{
+    cout<<"This is my simple code to save in the github";
+    
+}
+    
+}
